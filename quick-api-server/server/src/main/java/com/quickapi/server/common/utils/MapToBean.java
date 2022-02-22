684f2758ae02b541235d97c869f144f680e0bf71
yangxiao <1270009836@qq.com>
2022-02-22 21:58:29 +0800

    20220222-yangxiao-整合项目结构
    将quickapi-core分离为模块；拆分quick-api-test为测试项目

--- quick-api-server/server/src/main/java/com/quickapi/server/common/utils/MapToBean.java
+++ quick-api-server/server/src/main/java/com/quickapi/server/common/utils/MapToBean.java
@@ -1,8 +1,13 @@
 package com.quickapi.server.common.utils;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.toolkit.BeanUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.quickapi.server.exception.BusinessException;
+import org.apache.commons.collections4.MapUtils;
 
+import java.util.List;
 import java.util.Map;
 
 public class MapToBean {
@@ -23,5 +28,48 @@ public class MapToBean {
         }
 
         return  BeanUtils.mapToBean(map, beanClass);
+    }
+
+    public static <T> T toBean(Object object, Class<T> clazz) {
+        if (object == null) {
+            throw new BusinessException("MapToBean.toBean, object is null");
+        }
+        String jsonString = JSONObject.toJSONString(object);
+       return (T) JSONObject.parseObject(jsonString, clazz);
+    }
+
+    public static <T> List<T> toList(Object object, Class<T> clazz) {
+        String jsonString = JSONObject.toJSONString(object);
+        return JSONObject.parseArray(jsonString, clazz);
+    }
+
+    public static <T> Page<T> toPage(Object object, Class<T> clazz) {
+        Map<String, Object> map = (Map<String, Object>)object;
+        Page<T> page = new Page<>();
+        page.setTotal(MapUtils.getLong(map, "total", page.getTotal()));
+        page.setSize(MapUtils.getLong(map, "size", page.getSize()));
+        page.setCurrent(MapUtils.getLong(map, "current", page.getCurrent()));
+        page.setRecords(toList(MapUtils.getLong(map, "records"), clazz));
+
+       return page;
+    }
+
+    public static <T> Page<T> getPage(Map<String, Object> map, String key, Class<T> clazz) {
+        Map<String, Object> data = (Map<String, Object>)MapUtils.getObject(map, key);
+        if (MapUtils.isEmpty(data)) {
+            return null;
+        }
+
+        Page<T> page = new Page<>();
+        page.setSize(MapUtils.getLong(map, "size", page.getSize()));
+        page.setCurrent(MapUtils.getLong(map, "current", page.getCurrent()));
+
+        return page;
+    }
+
+    public static <T> T getObject(Map<String, Object> map, String key, Class<T> clazz) {
+        Object object = MapUtils.getObject(map, key);
+        String jsonString = JSONObject.toJSONString(object);
+        return JSONObject.parseObject(jsonString, clazz);
     }
 }
